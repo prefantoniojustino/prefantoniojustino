e9aa1484ccda5be8eaf014c9e126c484f353d6b9
Marcos Ferreira <marcosferreira.developer@gmail.com>
2024-08-29 19:31:19 -0300

    Add button install app

--- index.js
+++ index.js
@@ -71,6 +71,32 @@ const candidateList = [
     }
 ];
 
+// main.js
+
+let installPrompt = null;
+const installButton = document.querySelector("#install");
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  installPrompt = event;
+  installButton.removeAttribute("hidden");
+});
+
+installButton.addEventListener("click", async () => {
+    if (!installPrompt) {
+      return;
+    }
+    const result = await installPrompt.prompt();
+    console.log(`Install prompt was: ${result.outcome}`);
+    disableInAppInstallPrompt();
+  });
+  
+  function disableInAppInstallPrompt() {
+    installPrompt = null;
+    installButton.setAttribute("hidden", "");
+  }
+
+
 const candidates = document.getElementById("candidates");
 
 const loadCandidate = () => {
