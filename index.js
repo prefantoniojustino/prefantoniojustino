2ba0c52ae51eeef0a4d56e4c2f72d034b8b10b85
Marcos Ferreira <marcosferreira.developer@gmail.com>
2024-08-31 08:30:26 -0300

    Add item.candidate

--- index.js
+++ index.js
@@ -2,72 +2,86 @@ const candidateList = [
     {
         "image": "./assets/images/prefeito-02.png",
         "name": "ANTONIO JUSTINO",
-        "num": [4,0]
+        "num": [4,0],
+        "candidate": "Prefeito"
     },
     {
         "image": "./assets/images/VEREADORES/DAMÁSIO.png",
         "name": "DAMASIO BERTO",
-        "num": [4,0,0,0,0]
+        "num": [4,0,0,0,0],
+        "candidate": "Vereador(a)"
     },
     {
         "image": "./assets/images/VEREADORES/DENILSON.png",
         "name": "DENILSON ALVES",
-        "num": [4,0,1,2,3]
+        "num": [4,0,1,2,3],
+        "candidate": "Vereador(a)"
     },
     {
         "image": "./assets/images/VEREADORES/EDIVALDO.png",
         "name": "EDIVALDO ROCHA",
-        "num": [5,5,9,9,9]
+        "num": [5,5,9,9,9],
+        "candidate": "Vereador(a)"
     },
     {
         "image": "./assets/images/VEREADORES/GRACILENE.png",
         "name": "GRACILENE",
-        "num": [4,0,7,8,9]
+        "num": [4,0,7,8,9],
+        "candidate": "Vereador(a)"
     },
     {
         "image": "./assets/images/VEREADORES/LIZ-VITORIA.png",
         "name": "LIZ VITORIA",
-        "num": [5,5,7,7,7]
+        "num": [5,5,7,7,7],
+        "candidate": "Vereador(a)"
     },
     {
         "image": "./assets/images/VEREADORES/LUIZ-ALVES.png",
         "name": "LUIZ ALVES",
-        "num": [4,0,6,7,8]
+        "num": [4,0,6,7,8],
+        "candidate": "Vereador(a)"
     },
     {
         "image": "./assets/images/VEREADORES/NILMA.png",
         "name": "NILMA DE JOSELIO",
-        "num": [4,0,5,5,5]
+        "num": [4,0,5,5,5],
+        "candidate": "Vereador(a)"
     },
     {
         "image": "./assets/images/VEREADORES/RHUAN.png",
         "name": "RHUAN",
-        "num": [4,0,1,1,1]
+        "num": [4,0,1,1,1],
+        "candidate": "Vereador(a)"
     },
     {
         "image": "./assets/images/VEREADORES/ROGERIO-MOREIRA.png",
         "name": "ROGERIO MOREIRA",
-        "num": [4,0,3,3,3]
+        "num": [4,0,3,3,3],
+        "candidate": "Vereador(a)"
     },
     {
         "image": "./assets/images/VEREADORES/ROSINHA.png",
         "name": "ROSINHA BOLACHINHA",
-        "num": [4,0,8,8,8]
+        "num": [4,0,8,8,8],
+        "candidate": "Vereador(a)"
     },
     {
         "image": "./assets/images/VEREADORES/RUZIA.png",
         "name": "RUZIA BORGES",
-        "num": [4,0,4,4,4]
+        "num": [4,0,4,4,4],
+        "candidate": "Vereador(a)"
     },
     {
         "image": "./assets/images/VEREADORES/VAN-MACENA.png",
         "name": "VAN MACENA",
-        "num": [4,0,2,2,2]
+        "num": [4,0,2,2,2],
+        "candidate": "Vereador(a)"
     },
     {
         "image": "./assets/images/VEREADORES/VERIENE-VILARIM.png",
         "name": "VERIENE VILARIM",
-        "num": [5,5,6,6,6]
+        "num": [5,5,6,6,6],
+        "candidate": "Vereador(a)"
     }
 ];
 
@@ -112,7 +126,7 @@ const loadCandidate = () => {
                 <div class="data">
                     <div class="d-flex flex-column gap-2">
                         <div class="data-title">
-                            ${item.name} <small> candidato</small>
+                            ${item.name} <small> ${item.candidate}</small>
                         </div>
                         <div class="data-number">
                            ${numFinal.replaceAll(',', '')}
