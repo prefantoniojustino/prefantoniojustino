c422d44e5af7d3bd63423c06e35e4a40f1aab87a
Marcos Ferreira <marcosferreira.developer@gmail.com>
2024-09-13 14:51:45 -0300

    Update Workbox size file

--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,7 @@
 module.exports = {
-	globDirectory: '.',
-	globPatterns: [
-		'**/*.{jpeg,pdf,png,jpg,svg,txt,webm,css,html,js,json}'
-	],
-	swDest: 'sw.js',
-	ignoreURLParametersMatching: [
-		/^utm_/,
-		/^fbclid$/
-	]
+  globDirectory: ".",
+  globPatterns: ["**/*.{jpeg,pdf,png,jpg,svg,txt,webm,css,html,js,json}"],
+  swDest: "sw.js",
+  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
+  maximumFileSizeToCacheInBytes: 50000000, // <---- increasing the file size to cached 5mb
 };
